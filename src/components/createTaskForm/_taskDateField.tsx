c70d66776794f31206319cc213b31c485963a006
Manik <manik@pixelconception.com>
2023-02-18 16:34:41 +0530

    pushing lecture

--- src/components/createTaskForm/_taskDateField.tsx
+++ src/components/createTaskForm/_taskDateField.tsx
@@ -1,13 +1,21 @@
-import React, { FC, ReactElement, useState } from 'react';
+import React, { FC, ReactElement } from 'react';
 
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 import { DesktopDatePicker } from '@mui/x-date-pickers/DesktopDatePicker';
+import { IDateField } from './interfaces/IDateField';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import PropTypes from 'prop-types';
 import { TextField } from '@mui/material';
 
-export const TaskDateField: FC = (): ReactElement => {
-  // state
-  const [date, setDate] = useState<Date | null>(null);
+export const TaskDateField: FC<IDateField> = (
+  props,
+): ReactElement => {
+  //  Destructure props
+  const {
+    value = new Date(),
+    disabled = false,
+    onChange = (date) => console.log(date),
+  } = props;
 
   return (
     <>
@@ -15,8 +23,9 @@ export const TaskDateField: FC = (): ReactElement => {
         <DesktopDatePicker
           label="Task Date"
           inputFormat="dd/MM/yyyy"
-          value={date}
-          onChange={(newValue) => setDate(newValue)}
+          value={value}
+          onChange={onChange}
+          disabled={disabled}
           renderInput={(params) => (
             <TextField {...params} />
           )}
@@ -25,3 +34,5 @@ export const TaskDateField: FC = (): ReactElement => {
     </>
   );
 };
+
+TaskDateField.propTypes = {};
