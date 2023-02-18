aab95d2cbc5cb2b8d61a3ae72b91589e5e2fb79e
Manik <manik@pixelconception.com>
2023-02-18 16:57:01 +0530

    pushing lecture

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,11 +1,25 @@
 import React, { FC, ReactElement } from 'react';
 
 import { Box } from '@mui/material';
+import { ITask } from './interfaces/ITask';
+import { Priority } from '../createTaskForm/enums/Priority';
+import { Status } from '../createTaskForm/enums/Status';
 import { TaskDescription } from './_taskDescription';
 import { TaskFooter } from './_taskFooter';
 import { TaskHeader } from './_taskHeader';
 
-export const Task: FC = (props): ReactElement => {
+export const Task: FC<ITask> = (props): ReactElement => {
+  //  Destructure props
+  const {
+    title = 'Test Title',
+    date = new Date(),
+    description = 'Lorem ipsum dolor sit amet',
+    priority = Priority.normal,
+    status = Status.completed,
+    onStatusChange = (e) => console.log(e),
+    onClick = (e) => console.log(e),
+  } = props;
+
   return (
     <Box
       display="flex"
@@ -22,9 +36,12 @@ export const Task: FC = (props): ReactElement => {
         borderColor: 'error.light',
       }}
     >
-      <TaskHeader />
-      <TaskDescription />
-      <TaskFooter />
+      <TaskHeader title={title} date={date} />
+      <TaskDescription description={description} />
+      <TaskFooter
+        onClick={onClick}
+        onStatusChange={onStatusChange}
+      />
     </Box>
   );
 };
