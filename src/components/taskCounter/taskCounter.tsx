d0743714d7bd7bba9c301299131a0a413564fecd
Manik <manik@pixelconception.com>
2023-02-18 16:47:35 +0530

    pushing lecture

--- src/components/taskCounter/taskCounter.tsx
+++ src/components/taskCounter/taskCounter.tsx
@@ -2,8 +2,10 @@ import { Avatar, Box, Typography } from '@mui/material';
 import React, { FC, ReactElement } from 'react';
 
 import { ITaskCounter } from './interfaces/ITaskCounter';
+import PropTypes from 'prop-types';
 import { Status } from '../createTaskForm/enums/Status';
 import { emitCorrectBorderColor } from './helpers/emitCorrectBorderColor';
+import { emitCorrectLabel } from './helpers/emitCorrectLabel';
 
 export const TaskCounter: FC<ITaskCounter> = (
   props,
@@ -31,7 +33,7 @@ export const TaskCounter: FC<ITaskCounter> = (
           }}
         >
           <Typography color="#ffffff" variant="h4">
-            10
+            {count}
           </Typography>
         </Avatar>
         <Typography
@@ -40,9 +42,18 @@ export const TaskCounter: FC<ITaskCounter> = (
           fontSize="20px"
           variant="h5"
         >
-          Subtitle
+          {emitCorrectLabel(status)}
         </Typography>
       </Box>
     </>
   );
+};
+
+TaskCounter.propTypes = {
+  count: PropTypes.number,
+  status: PropTypes.oneOf([
+    Status.todo,
+    Status.inProgress,
+    Status.completed,
+  ]),
 };
